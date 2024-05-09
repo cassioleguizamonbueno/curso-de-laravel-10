0e6abf36cc3d48fc4fff7db111933401438a069b
Cassio Bueno <cassioleguizamonbueno@gmail.com>
2024-05-09 02:01:32 -0300

    correcoes nas migrations

--- database/migrations/2024_05_09_012309_pedidos_status.php
+++ database/migrations/2024_05_09_012309_pedidos_status.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,6 +17,23 @@ return new class extends Migration
             $table->string("descricao", 255)->nullable(true);
             $table->timestamps();
         });
+
+        DB::table('pedidos_status')->insert(
+            array(
+                [
+                    'descricao' => 'Solicitado',
+                    'created_at' => now()
+                ],
+                [
+                    'descricao' => 'Concluído',
+                    'created_at' => now()
+                ],
+                [
+                    'descricao' => 'Cancelado',
+                    'created_at' => now()
+                ],
+            )
+        );
     }
 
     /**
